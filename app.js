87840169b4a8242058cd192ea066bb67ab30b2df
leomrg861 <leomrg861@gmail.com>
2018-11-12 22:23:01 +0200

    pug

--- app.js
+++ app.js
@@ -13,9 +13,13 @@ app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dis
 app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
 app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist')));
 
+app.set('views', './src/views');
+app.set('view engine', 'pug');
+
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views', 'index.html'));
+  //  res.sendFile(path.join(__dirname, 'views', 'index.html'));
+  res.render('index', { list: ['a', 'b'] });
 });
 
-const port = 3000;
-app.listen(port, () => debug(`listening on port ${chalk.green(port)}`));
+const port = process.env.PORT || 3000;
+app.listen(port, () => debug(`listening at port ${chalk.green(port)}`));
