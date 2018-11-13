7f1aaa01ee5213297dcbe2d18d6aa980dab35c08
leomrg861 <leomrg861@gmail.com>
2018-11-13 18:22:22 +0200

    add books router to encapsulate books routs in this router

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const path = require('path');
 
 const app = express();
 
+const booksRouter = express.Router();
+
 app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
@@ -17,12 +19,29 @@ app.set('views', './src/views');
 //  app.set('view engine', 'pug');
 app.set('view engine', 'ejs');
 
+booksRouter.route('/')
+  .get((req, res) => {
+    res.send('hello books');
+  });
+
+booksRouter.route('/single')
+  .get((req, res) => {
+    res.send('hello single book');
+  });
+
+app.use('/books', booksRouter);
+
 app.get('/', (req, res) => {
   //  res.sendFile(path.join(__dirname, 'views', 'index.html'));
-  res.render('index', {
-    list: ['a', 'b', 'cc'],
-    title: 'Library',
-  });
+  res.render(
+    'index',
+    {
+      nav: [
+        { link: '/books', title: 'Books' },
+        { link: '/authors', title: 'Authors' }],
+      title: 'Library'
+    }
+  );
 });
 
 const port = process.env.PORT || 3000;
