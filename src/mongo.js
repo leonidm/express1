44bb22872b506071d6e9a34f11b34ed730b88800
leomrg861 <leomrg861@gmail.com>
2018-11-19 18:25:19 +0200

    Set url and dbName in static  class  properties

--- src/mongo.js
+++ src/mongo.js
@@ -5,21 +5,19 @@ const debug = require('debug')('app:mongoUtils');
 
 class Mongo {
 
-  constructor() {
-    this.url = 'mongodb://localhost:27017';
-    this.dbName = 'libraryApp';
-  }
-
   async getCollection(collectionName) {
 
-    const client = await MongoClient.connect(this.url);
+    const client = await MongoClient.connect(Mongo.url);
     debug('Connected to mongo server');
 
-    const db = client.db(this.dbName);
+    const db = client.db(Mongo.dbName);
     const collection = await db.collection(collectionName);
 
     return { client, collection };
   }
 }
+
+Mongo.url = 'mongodb://localhost:27017';
+Mongo.dbName = 'libraryApp';
 
 module.exports = new Mongo();
